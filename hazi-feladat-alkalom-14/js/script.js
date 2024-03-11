01dbb28a55ba1f367ebf76ca03d877189996e14f
Mutz Dezső <mutz.dezso@yahoo.com>
2024-03-11 13:48:18 +0100

    collected all homework 02

---
+++ hazi-feladat-alkalom-14/js/script.js
@@ -0,0 +1,9 @@
+function mutat() {
+    document.getElementById("fo-lap").style.display = "block";
+    document.getElementById("fo-lap").style.width = "100px";
+    document.getElementById("fo-lap").style.height = "50px";
+}
+
+function elrejt() {
+    document.getElementById("fo-lap").style.display = "none";
+}
