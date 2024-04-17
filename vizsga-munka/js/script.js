14a26dd639b8d4950b6dac0457df451ea970ce45
Mutz Dezső <mutz.dezso@yahoo.com>
2024-04-17 13:06:57 +0200

    exam work 1.7

--- vizsga-munka/js/script.js
+++ vizsga-munka/js/script.js
@@ -2,21 +2,21 @@ const menu = document.querySelector(".menu");
 //const menuItem = document.querySelector(".menuItem");
 const hamburgerMenu = document.querySelector(".hamburger-menu");
 const bezaroGomb = document.querySelector(".bezaro-gomb");
-const bezaroGombNyitolap = document.querySelector(".bezaro-gomb-nyito");
-const logo = document.querySelector(".logo a");
+//const bezaroGombNyitolap = document.querySelector(".bezaro-gomb-nyito");
+//const logo = document.querySelector(".logo a");
 
 const menuKapcsolo = ()=> {
     if(menu.classList.contains("menuMutatas")) {
         menu.classList.remove("menuMutatas");
         hamburgerMenu.style.display = "block";
         bezaroGomb.style.display = "none";
-        bezaroGombNyitolap.style.display = "none";
+        //bezaroGombNyitolap.style.display = "none";
     }
     else {
         menu.classList.add("menuMutatas");
         hamburgerMenu.style.display = "none";
         bezaroGomb.style.display = "block";
-        bezaroGombNyitolap.display = "block";
+        //bezaroGombNyitolap.display = "block";
         //logo.style.display ="block";
     }
 }
